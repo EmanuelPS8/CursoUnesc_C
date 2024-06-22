89533b4a2e46a833cb65ab00e121007793c9fb68
EmanuelPS8 <97064304+EmanuelPS8@users.noreply.github.com>
2024-06-21 21:28:50 -0300

    test

---
+++ ex1_vetores.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+    int num[7];
+    for (int i = 0; i < 7; i++)
+    {
+        printf("Digite um numero: ");
+        scanf("%d", &num[i]);
+        
+    }
+    printf("%d\t",num);
+    
+}
